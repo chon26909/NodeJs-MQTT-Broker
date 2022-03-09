c3f4fb27724c70ede378061ab66f6c52aefb9c7d
chon26909 <chon26909@gmail.com>
2022-03-09 10:18:34 +0700

    add authenticate

--- broker.js
+++ broker.js
@@ -1,18 +1,26 @@
 const mosca = require('mosca');
-
-
 const PORT = process.env.PORT || 4000
 
 const settings = {
-    port: PORT ,
+    port: PORT,
 }
 
 const broker = new mosca.Server(settings);
 
 broker.on('ready', () => {
-    console.log('Broker is ready!')
+    console.log('Server is ready!');
+    broker.authenticate = authenticate;
 })
 
+const authenticate = (client, username, password, callback) => { 
+    const authorized = (username == 'chon' && password.toString() == '1234')
+    if(authorized) client.user == username;
+    callback(null, authorized);
+}
+ 
 broker.on('published', (packet) => { 
-    console.log(String(packet.payload))
+    // console.log(packet);
+    console.log('topic : ', String(packet.topic))
+    console.log('message : ', String(packet.payload))
+    console.log(' ')
 })
