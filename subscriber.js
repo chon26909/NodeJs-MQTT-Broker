3ada09268b53696bc94e11d6e9a52b2e5cf76429
chon26909 <chon26909@gmail.com>
2022-03-09 10:19:12 +0700

    edit subscriber

--- subscriber.js
+++ subscriber.js
@@ -1,17 +1,19 @@
 const mqtt = require('mqtt');
 
-const client = mqtt.connect('mqtt://localhost:1234');
-
-const topic = 'count';
+const client = mqtt.connect('http://192.168.1.150:4000');
 
 client.on('message', (topic, message) => { 
 
-    if (topic === 'count') console.log('count -> ', String(message)) 
-    else if (topic === 'p') console.log('----------------- p ->', String(message));
+    // console.log(String(topic), String(message))
+
+    if (topic === 'room/light1') console.log('light 1 ') 
+    else if (topic === 'room/light2') console.log('light 2 ') 
+    else if (topic === 'room/light3') console.log('light 3 ') 
 
 })
 
 client.on('connect', () => { 
-    client.subscribe('count')
-    client.subscribe('p')
+    client.subscribe('room/light1')
+    client.subscribe('room/light2')
+    client.subscribe('room/light3')
 })
